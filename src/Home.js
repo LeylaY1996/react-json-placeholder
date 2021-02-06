46be249901ec959815e8177dd3b94ed73a4bf848
LeylaY1996 <leylay1996@gmail.com>
2021-02-06 21:59:20 +0300

    added home page1

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="col">
+                <div className="col-md-6">
+                    <p>User 1</p>
+                </div>
+                <div className="col-md-6">
+                    <p>User 1</p>
+                </div>
+                <div className="col-md-6">
+                    <p>User 1</p>
+                </div>
+                <div className="col-md-6">
+                    <p>User 1</p>
+                </div>
+                <div className="col-md-6">
+                    <p>User 1</p>
+                </div>
+                <div className="col-md-6">
+                    <p>User 1</p>
+                </div>
+                <div className="col-md-6">
+                    <p>User 1</p>
+                </div>
+                <div className="col-md-6">
+                    <p>User 1</p>
+                </div>
+            </div>
+        </div>
+    )
+}
